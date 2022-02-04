924bb33750aacfd04d4fd00ec388b37f84171010
jorelljao <jorelljao@gmail.com>
2022-02-04 23:49:50 +0800

    Update App.test.tsx

--- landing-page/src/App.test.tsx
+++ landing-page/src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import Coinica from './Coinica';
 
 test('renders learn react link', () => {
