31f5c47dc41b2ba0f5361f7e901b640dbae40f73
jorelljao <jorelljao@gmail.com>
2022-02-04 23:33:27 +0800

    remove metamask call

--- landing-page/src/App.test.tsx
+++ landing-page/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Coinica from './Coinica';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Coinica />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
